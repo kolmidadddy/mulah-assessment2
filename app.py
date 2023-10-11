efd44361c80abaa796e2c4b4f039b22b32c42014
kolmidaddy <samanthaltw1221@1utar.my>
2023-10-11 17:45:19 +0800

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+import secrets
 
 app = Flask(__name__)
+# Generate a random secret key
+secret_key = secrets.token_hex(16)
+
+# Set the secret key for the Flask app
+app.secret_key = secret_key
+
 
 @app.route('/')
 def index():
